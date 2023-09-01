d4698d89afb502b0984995d27a6cbb9c9f357461
1FaKe110 <gabko2016@gmail.com>
2023-09-01 11:46:31 +0300

    fixed fix_me.py

--- fix_me.py
+++ fix_me.py
@@ -1,8 +1,24 @@
-def calculate_average(nums):
-    total = sum(nums)
-    count = len(numbers)
-    average = total / count
+"""Get average module"""
 
-nums = [10, 15, 20]
-result = calculat_average(nums)
-Print("The average is:", result)
+
+def calculate_average(nums):
+    """
+    Get average from list
+    :param nums: List[int]
+    :return: float
+    """
+    return sum(nums) / len(nums)
+
+
+def main():
+    """
+    main function of module
+    :return: None
+    """
+    nums = [10, 15, 20]
+    result = calculate_average(nums)
+    print("The average is:", result)
+
+
+if __name__ == '__main__':
+    main()
